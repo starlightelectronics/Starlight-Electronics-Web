9571e4f95e1f1824a94c90af17370085b7611978
Yuberley <yuberleyguerrero1999@gmail.com>
2022-05-11 09:56:59 -0500

    Configuration tailwindcss and dev environment

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,26 @@
 import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import './index.css'
 
-function App() {
+
+const App = () => {
+
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <header className="py-6">
+      <div className="container flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full">
+          <div className="text-lg font-bold">Startlight Electronics</div>
+          <div className="hidden md:flex space-x-12 items-center">
+              <a href="#" className="text-selected-text">Home</a>
+              <a href="#work">My work</a>
+              <a href="#clients">Clients</a>
+              <a href="#hire"><button className="px-6 py-2 bg-theme font-bold">Hire me</button></a>
+          </div>
+          <div className="md:hidden">
+              <svg width="26" height="18" viewBox="0 0 26 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13 17.5H0.25V14.6667H13V17.5ZM25.75 10.4167H0.25V7.58333H25.75V10.4167ZM25.75 3.33333H13V0.5H25.75V3.33333Z" fill="white"/></svg>
+          </div>
+      </div>
+  </header>
   )
 }
 
