b847e5ba08e13fa0f059ec53941af5918217bf9d
Yuberley <yuberleyguerrero1999@gmail.com>
2022-05-11 19:40:28 -0500

    try add a form with netlify

--- src/App.jsx
+++ src/App.jsx
@@ -197,7 +197,7 @@ const App = () => {
 					<p className="section-paragraph"> Lorem ipsum dolor sit amet consectetur adipisicing elit. In, consectetur. </p>
 
 					<div className="w-full grid lg:grid-cols-2 gap-8 lg:gap-32 mt-24">
-						<form className="space-y-12" netlify>
+						<form className="space-y-12" data-netlify="true" method="POST" name="contact">
 							<div>
 								<label className="text-white block mb-6 text-xl font-bold">Name</label>
 								<input className="w-full border border-input-border bg-input px-4 py-4" />
@@ -210,7 +210,7 @@ const App = () => {
 								<label className="text-white block mb-6 text-xl font-bold">Message</label>
 								<textarea type="email" className="w-full border border-input-border bg-input px-4 py-4 h-56 resize-none"></textarea>
 							</div>
-							<button className="px-6 py-2 bg-theme text-white font-bold">Send it!</button>
+							<button type='submit' className="px-6 py-2 bg-theme text-white font-bold">Send it!</button>
 						</form>
 
 						<div className="mt-12">
