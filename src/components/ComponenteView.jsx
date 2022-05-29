8353581597ded8580e47cd04c7764a1d97d83e74
Yuberley <yuberleyguerrero1999@gmail.com>
2022-05-29 00:54:01 -0500

    services and markets modified

--- src/components/ComponenteView.jsx
+++ src/components/ComponenteView.jsx
@@ -28,7 +28,7 @@ export const ComponenteView = ({ service, index }) => {
         // </section>
         <section className="py-20 animate__animated animate__fadeIn border-b-2 sm:border-none">
             <fieldset className="container items-center max-w-screen-xl 4xl:max-w-screen-2xl px-10 mx-auto sm:px-20 md:px-32 lg:px-16 4xl:px-10 sm:border sm:border-dashed sm:rounded-xl sm:pt-8 sm:pb-16">
-                <legend className='px-10 text-center mb-5 text-3xl font-bold leading-tight tracking-tight sm:text-4xl font-heading uppercase'>{ service.title }</legend>
+                <legend className='sm:px-10 text-center mb-5 text-3xl font-bold leading-tight tracking-tight sm:text-4xl font-heading uppercase'>{ service.title }</legend>
                 <div className="flex flex-wrap items-center -mx-3 4xl:mx-0">
                     <div className={`w-full px-3 mb-12 lg:w-1/2 order-0 ${ (index % 2) == 0 ? 'lg:order-2' : 'lg:order-1' } lg:mb-0`}>
                         <img className="mx-auto sm:max-w-sm lg:max-w-full rounded-lg hover:shadow-2xl hover:scale-110 transition duration-300" src={ service.image } alt="image starlight electronics" />
