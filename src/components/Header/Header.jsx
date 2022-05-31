8a3e0a18475a75c49411e4e69af5ad109ab21640
Yuberley <yuberleyguerrero1999@gmail.com>
2022-05-30 22:47:43 -0500

    SEO Configuration

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -26,7 +26,7 @@ export const Header = ({ setPage = 'home' }) => {
 		<header className="py-6">
 			<div className="container flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full">
 				<div className='flex justify-items-center items-center'>
-					<a href="">
+					<a href="/">
 						<img src={ logo_sle_white } className="h-20" alt="logo starlight electronics" title='logo starlight electronics' />
 					</a>
 				</div>
@@ -95,11 +95,11 @@ export const Header = ({ setPage = 'home' }) => {
 				</div>
 
 				<div className="block lg:hidden w-1/5 lg:w-4/6">
-					<a ref={ menuToogle } className='md:hidden text-white block p-5 font-bold'>
+					<div ref={ menuToogle } className='md:hidden text-white block p-5 font-bold'>
 						<button onClick={ handleClickMenu }>
-							<svg width="26" height="18" viewBox="0 0 26 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13 17.5H0.25V14.6667H13V17.5ZM25.75 10.4167H0.25V7.58333H25.75V10.4167ZM25.75 3.33333H13V0.5H25.75V3.33333Z" fill="white"/></svg>
+							<svg width="36" height="36" viewBox="0 0 26 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13 17.5H0.25V14.6667H13V17.5ZM25.75 10.4167H0.25V7.58333H25.75V10.4167ZM25.75 3.33333H13V0.5H25.75V3.33333Z" fill="white"/></svg>
 						</button>
-					</a>
+					</div>
 
 					{
 						showMenu ? 
