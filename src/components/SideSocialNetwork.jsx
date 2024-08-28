e82523e0b30374ed59822b7e6789a9c76fd3b3e8
starlightelectronics <105379087+starlightelectronics@users.noreply.github.com>
2024-08-28 09:41:02 -0500

    Update SideSocialNetwork.jsx

--- src/components/SideSocialNetwork.jsx
+++ src/components/SideSocialNetwork.jsx
@@ -26,7 +26,7 @@ export const SideSocialNetwork = () => {
             <a
               className='flex justify-between items-center w-full mr-3 ml-3 text-gray-300'
               target="_blank"
-              href='https://www.facebook.com/StarlightElectronics'
+              href='https://www.facebook.com/people/Starlight-Electronics-SAS/61564271327740/?mibextid=LQQJ4d&rdid=SxbKmK42KqpeAEWG&share_url=https%253A%252F%252Fwww.facebook.com%252Fshare%252FRhdx78MpajhnHD6B%252F%253Fmibextid%253DLQQJ4d&checkpoint_src=any '
             >
               <FaFacebook size={30} /> Facebook 
             </a>
@@ -44,7 +44,7 @@ export const SideSocialNetwork = () => {
             <a
               className='flex justify-between items-center w-full mr-3 ml-3 text-gray-300'
               target="_blank"
-              href='https://www.linkedin.com/in/starlight-electronics-s-a-s-421a2a190/'
+              href='https://www.linkedin.com/company/104921825/admin/dashboard/'
             >
               <FaLinkedin size={30} /> LinkedIn 
             </a>
