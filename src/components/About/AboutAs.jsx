53b8018cd4cf1629a05bd480fb95ceba266ca407
starlightelectronics <105379087+starlightelectronics@users.noreply.github.com>
2026-02-11 16:33:42 -0500

    Update AboutAs.jsx

--- src/components/About/AboutAs.jsx
+++ src/components/About/AboutAs.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import SLE_ISO from '../../assets/img/content/Firma-bureauveritas -2025.gif'
+import SLE_ISO from '../../assets/img/content/ISO_ISO90012015.png'
 import mision from '../../assets/img/gallery/img_49.webp'
 import vision from '../../assets/img/gallery/img_27.webp'
 
