8cf994851897f93fdd413d1578f6e9c3ceb45642
starlightelectronics <105379087+starlightelectronics@users.noreply.github.com>
2026-05-28 12:41:26 -0500

    FormContact.jsx

--- src/components/FormContact.jsx
+++ src/components/FormContact.jsx
@@ -29,23 +29,30 @@ export const FormContact = () => {
                         <button type='submit' className="px-6 py-2 bg-theme text-white font-bold">Enviar</button>
                     </form>
                 
-                        <map name="globalposition">
-                            <area shape="rect" coords="190,310,232,350" href="https://www.controlavllc.com/" alt="positionglobal" target='_blank' />
-                            <area shape="rect" coords="220,370,240,420" href="#home" alt="positionglobal" />
-                            <area shape="rect" coords="380,290,420,330" href="#home" alt="positionglobal" />
-                            <area shape="rect" coords="435,210,475,250" href="https://www.controlavllc.com/" alt="positionglobal" target="_blank" />
-                        </map>
+                    <map name="globalposition">
+                        <area shape="rect" coords="190,310,232,350" href="https://www.controlavllc.com/" alt="positionglobal" target='_blank' />
+                        <area shape="rect" coords="220,370,240,420" href="#home" alt="positionglobal" />
+                        <area shape="rect" coords="380,290,420,330" href="#home" alt="positionglobal" />
+                        <area shape="rect" coords="435,210,475,250" href="https://www.controlavllc.com/" alt="positionglobal" target="_blank" />
+                    </map>
 
-                        <figure className='my-auto w-full border-dashed border-2 rounded-xl 4xl:mb-32'>
-                            <img src={ globalpositionwhite } alt="mapa de posicion global de starlightelectronics y controlavllc" className="md:h-2/4 mx-auto md:mt-5 4xl:w-8/12" useMap='#globalposition' />
-                        </figure>
+                    <figure className='my-auto w-full border-dashed border-2 rounded-xl 4xl:mb-32 relative'>
+                        <img src={ globalpositionwhite } alt="mapa de posicion global de starlightelectronics y controlavllc" className="md:h-2/4 mx-auto md:mt-5 4xl:w-8/12" useMap='#globalposition' />
+                        <div className="absolute bottom-4 left-4 text-white text-xs md:text-sm">
+                            <ul className="space-y-1">
+                                <li className="font-bold">• EUROPA</li>
+                                <li className="ml-4">○ Totana, España</li>
+                                <li className="ml-4">○ Turku, Finlandia</li>
+                                <li className="font-bold mt-1">• AMERICA DEL NORTE</li>
+                                <li className="ml-4">○ Miami, Florida</li>
+                                <li className="font-bold mt-1">• AMERICA DEL SUR</li>
+                                <li className="ml-4">○ Bogotá D.C, Colombia</li>
+                                <li className="ml-4">○ Villavicencio, Meta - Colombia</li>
+                            </ul>
+                        </div>
+                    </figure>
                     
                 </div>
-
-                {/* <div id="toast-simple" className="flex items-center w-full max-w-xs p-4 mt-10 space-x-4 text-gray-500 bg-white divide-x divide-gray-200 rounded-lg shadow dark:text-gray-400 dark:divide-gray-700 space-x dark:bg-gray-800" role="alert">
-                    <svg className="w-5 h-5 text-blue-600 dark:text-sky-600" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="paper-plane" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M511.6 36.86l-64 415.1c-1.5 9.734-7.375 18.22-15.97 23.05c-4.844 2.719-10.27 4.097-15.68 4.097c-4.188 0-8.319-.8154-12.29-2.472l-122.6-51.1l-50.86 76.29C226.3 508.5 219.8 512 212.8 512C201.3 512 192 502.7 192 491.2v-96.18c0-7.115 2.372-14.03 6.742-19.64L416 96l-293.7 264.3L19.69 317.5C8.438 312.8 .8125 302.2 .0625 289.1s5.469-23.72 16.06-29.77l448-255.1c10.69-6.109 23.88-5.547 34 1.406S513.5 24.72 511.6 36.86z"></path></svg>
-                    <div className="pl-4 text-sm font-normal">Mensaje enviado.</div>
-                </div> */}
 
                 <div id='location' className="container bg-[#0f1d40] text-gray-800 px-4 md:px-12 pt-7 rounded-2xl mb-20 mt-20">
                     <div className="block py-10 md:py-12 px-4 md:px-6">
