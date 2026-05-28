5aecff391e262453a535dc4b3b96c308b865ad92
starlightelectronics <105379087+starlightelectronics@users.noreply.github.com>
2026-05-28 15:55:25 -0500

    HeaderMobile.jsx

--- src/components/Header/HeaderMobile.jsx
+++ src/components/Header/HeaderMobile.jsx
@@ -3,14 +3,12 @@ import React from 'react';
 export const HeaderMobile = ({ title, value, handleInputHeader }) => {
 	return (
 		<li>
-            <a href="#" className="text-white block p-5 font-bold hover:text-sky-700">
-                <button 
-                    className='hover:scale-110 transition duration-200 hover:text-sky-700 font-bold'
-                    value={ value }
-                    onClick={ handleInputHeader } >
-                    { title }
-                </button>
-            </a>
+            <button 
+                className='text-white hover:scale-110 transition duration-200 hover:text-sky-400 font-bold w-full py-4'
+                value={ value }
+                onClick={ handleInputHeader } >
+                { title }
+            </button>
         </li>
 	);
 };
