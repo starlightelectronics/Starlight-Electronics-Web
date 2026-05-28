81497a8c8fc84c631253559c9e89c1acf68c535f
starlightelectronics <105379087+starlightelectronics@users.noreply.github.com>
2026-05-28 16:31:45 -0500

    SideSocialNetwork.jsx

--- src/components/SideSocialNetwork.jsx
+++ src/components/SideSocialNetwork.jsx
@@ -18,15 +18,7 @@ export const SideSocialNetwork = () => {
               className='flex justify-between items-center w-full mr-3 ml-3 text-gray-300'
               target="_blank"
               href='https://wa.me/573173759475'
-            >
-             <FaWhatsapp size={30} /> Whatsapp 
-            </a>
-          </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center hover:ml-[-100px] ml-[-0px] duration-300 bg-theme'>
-            <a
-              className='flex justify-between items-center w-full mr-3 ml-3 text-gray-300'
-              target="_blank"
-              href='https://www.facebook.com/people/Starlight-Electronics-SAS/61564271327740/?mibextid=LQQJ4d&rdid=SxbKmK42KqpeAEWG&share_url=https%253A%252F%252Fwww.facebook.com%252Fshare%252FRhdx78MpajhnHD6B%252F%253Fmibextid%253DLQQJ4d&checkpoint_src=any '
+            
             >
               <FaFacebook size={30} /> Facebook 
             </a>
