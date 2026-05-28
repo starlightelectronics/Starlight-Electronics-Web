7aa0b6e2c348b3ffdcf65a91d68591dc12bbde7a
starlightelectronics <105379087+starlightelectronics@users.noreply.github.com>
2026-05-28 16:48:50 -0500

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ const App = () => {
 						<SideSocialNetwork />
 						<div className="container mt-10 flex justify-between items-center mx-auto px-8 md:px-14 lg:px-6 w-full">
 							<div className="flex flex-wrap md:flex-nowrap" data-aos="fade-up">
-								<Home />
+								<Home setPage={ setPage } />
 							</div>
 						</div>
 						<TextInfo />
