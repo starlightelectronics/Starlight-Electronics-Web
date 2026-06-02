7e5bda8c31b70f085de07c34010b53835e3906a0
starlightelectronics <105379087+starlightelectronics@users.noreply.github.com>
2026-06-02 11:37:06 -0500

    Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -119,7 +119,7 @@ export const Home = ({ setPage }) => {
             </div>
 
             {/* Carrusel automático de fotos */}
-            <div className="my-auto mx-auto mt-12 mr-0 sm:mr-10 sm:mt-auto 4xl:mt-20 relative" style={{minHeight: '400px', minWidth: '300px'}}>
+            <div className="my-auto mt-12 sm:mt-auto 4xl:mt-20 relative" style={{minHeight: '400px', minWidth: '300px', marginLeft: 'auto', marginRight: '0'}}>
                 { bgImages.map((img, i) => (
                     <img
                         key={i}
